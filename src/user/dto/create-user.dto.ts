f2da7bd5e361bc3a1261628a77c23d21d91b5a01
SokCheasrouch <cheasrouch.sok@gmail.com>
2022-10-19 23:26:37 +0700

    open api

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+export class CreateUserDto {
+    @ApiProperty({
+        description: 'Full name',
+        example: "Joe"
+      })
+    userName:string;
+    
+    @ApiProperty({
+        description: 'Has to match a regular expression: /^\\+[1-9]\\d{1,14}$/',
+        example: '+123123123123'
+      })
+    phone:string;
+}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { prop } from '@typegoose/typegoose';
+import { Document } from 'mongoose';
+
+export type CatDocument = User & Document;
+export class User {
+    @prop({required: [true, "User name is required"]})
+    userName:string;
+
+    @prop({required: [true, "Phone is required"]})
+    phone:string;
+}
+
+export const ChatSchema = SchemaFactory.createForClass(User);
+
